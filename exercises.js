b14f139bee621d8de2bd776a4133fd48d1a0d98a
Tarik Desire <tprdesire@gmail.com>
2018-10-04 14:44:33 -0400

    Added displaySize function

--- exercises.js
+++ exercises.js
@@ -8,6 +8,21 @@ function displayList(list) {
   console.log(JSON.stringify(list, null, 2));
 }
 
+function displaySize(list) {
+  if(!list.head) {
+    console.log('The linked list is empty');
+    return;
+  }
+  
+  let count = 1;
+  let tempNode = list.head;
+  while (tempNode.next !== null) {
+    count++;
+    tempNode = tempNode.next;
+  }
+  console.log(`The linked list has '${count}' nodes`);
+}
+
 function main() {
   const SLL = LL;
 
@@ -22,6 +37,7 @@ function main() {
   SLL.insertAt('Kat', 3);
   SLL.remove('Tauhida');
   displayList(SLL);
+  displaySize(SLL);
 }
 
 main();
